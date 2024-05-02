a2ea06274c9b4869f222452573f65c12dad3a166
draquio <draquio@gmail.com>
2024-05-02 17:21:08 -0400

    proyecto finalizado

---
+++ SistemaVenta.API/Controllers/RolController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.API.Utilidad;
+
+namespace SistemaVenta.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolController : ControllerBase
+    {
+        private readonly IRolService _rolServicio;
+
+        public RolController(IRolService rolService)
+        {
+            _rolServicio = rolService;
+        }
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var rsp = new Response<List<RolDTO>>();
+            try {
+                rsp.status = true;
+                rsp.value = await _rolServicio.Lista();
+
+            }
+            catch (Exception ex) { 
+                rsp.status = false;
+                rsp.msg = ex.Message;
+            }
+            return Ok(rsp);
+        }
+
+    }
+}
